4ea90a41c98d470ec8d37c84205b76ea6aa7499b
quckh <quckh@KHANH>
2022-05-20 12:54:31 +0700

    Add project files.

---
+++ PRN_SE1624_OOP/Employees.cs
@@ -0,0 +1,39 @@
+﻿namespace Prn.Se1624;
+public class Employee
+{
+    //Define fields-cac data
+    private int _id;
+    private string _name;
+    private DateOnly _dob;
+
+    //Define properties
+    public float Salary { get; set; }
+    public string Level { get; set; }
+
+    public int Id { get => _id; set => _id = value; }
+    public string Name { get => _name; set => _name = value; }
+    public DateOnly Dob { get => _dob; set => _dob = value; }
+
+    //Constructors
+    public Employee()
+    {
+
+    }
+    public Employee(int id, string name, DateOnly dob, float salary, string level)
+    {
+        Id = id;
+        Name = name;
+        Dob = dob;
+        Salary = salary;
+        Level = level;
+    }
+    //manual methods
+    public override string? ToString()
+    {
+        return _getString() ;
+    }
+    private string _getString()
+    {
+        return $"[Id = {this._id}], Name = {Name}, Dob = {this._dob}, Level = {Level}, Salary = {Salary}";
+    }
+}
---
+++ PRN_SE1624_OOP/Program.cs
@@ -0,0 +1,21 @@
+﻿// lap trinh tao ra cac list employee
+namespace Prn.Se1624;
+
+public class Program
+{
+    static void Main(String[] args)
+    {
+        //1. create a object(instance)
+        Employee e = new Employee();
+        e.Id = 1;
+        e.Name = "Nguyen Quoc Khanh";
+        e.Level = "Junior";
+        e.Salary = 5000;
+        e.Dob = new DateOnly();
+        e.ToString();
+        Console.WriteLine(e);
+        //create a list emplotee and filter and display to console
+        Console.WriteLine(e.ToString());
+        Console.ReadLine();
+    }
+}
